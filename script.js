b5d3beb08481e7fe5fc69c21b8223341c7bc04dd
Rabiul Hassan Jomman <75951818+RabiulHassanJomman@users.noreply.github.com>
2025-05-22 14:54:40 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,191 @@
+const gridElement = document.getElementById("grid")
+const gameOver = document.getElementById("game-over-container")
+const finalScore = document.getElementById("final-score");
+const scoreCount = document.getElementById("score-count")
+let grid;
+let score;
+
+let touchStartX;
+let touchStartY;
+let touchEndX;
+let touchEndY;
+
+document.addEventListener("touchstart", (e) => {
+  touchStartX = e.touches[0].screenX;
+  touchStartY = e.touches[0].screenY;
+})
+
+document.addEventListener("touchend", (e) => {
+  touchEndX = e.changedTouches[0].screenX;
+  touchEndY = e.changedTouches[0].screenY;
+  
+  handleTouch();
+})
+
+function startGame() {
+  grid = Array(4).fill().map(() => Array(4).fill(0))
+  score = 0;
+  
+  addRandomTile();
+  addRandomTile();
+  
+  updateGrid()
+}
+
+function addRandomTile() {
+  let emptyCells = [];
+  
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      if (grid[i][j] === 0) {
+        emptyCells.push({ row: i, col: j })
+      }
+    }
+  }
+  if (emptyCells.length > 0) {
+    const { row, col } = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+    grid[row][col] = Math.random() < 0.9 ? 2 : 4;
+  }
+}
+
+function move(direction) {
+  let moved = false;
+  
+  
+  if (direction === "left") {
+    let row = [];
+    for (i = 0; i < 4; i++) {
+      row = grid[i].filter((value) => value !== 0);
+      for (j = 0; j < row.length - 1; j++) {
+        if (row[j] === row[j + 1]) {
+          row[j] *= 2;
+          row[j + 1] = 0;
+          score += row[j];
+        }
+      }
+      row.filter((value) => value !== 0);
+      while (row.length < 4) row.push(0);
+      if (grid[i].join() !== row.join())
+        moved = true;
+      grid[i] = row;
+    }
+  }
+  
+  
+  else if (direction === "right") {
+    let row = [];
+    for (i = 0; i < 4; i++) {
+      row = grid[i].filter((value) => value !== 0);
+      for (j = row.length - 1; j > 0; j--) {
+        if (row[j] === row[j - 1]) {
+          row[j] *= 2;
+          row[j - 1] = 0;
+          score += row[j];
+        }
+      }
+      row.filter((value) => value !== 0);
+      while (row.length < 4) row.unshift(0);
+      if (grid[i].join() !== row.join())
+        moved = true;
+      grid[i] = row;
+    }
+  }
+  
+  else if (direction === "up") {
+    let col = [];
+    for (i = 0; i < 4; i++) {
+      col = [grid[0][i], grid[1][i], grid[2][i], grid[3][i], ].filter((value) => value !== 0);
+      for (let j = 0; j < col.length; j++) {
+        if (col[j] === col[j + 1]) {
+          col[j] *= 2;
+          col[j + 1] = 0;
+          score += col[j];
+        }
+      }
+      col.filter((value) => value !== 0);
+      while (col.length < 4) col.push(0);
+      for (j = 0; j < col.length; j++) {
+        if (grid[i][j] !== col[j]) moved = true;
+        grid[j][i] = col[j];
+      }
+    }
+  }
+  
+  else if (direction === "down") {
+    let col = [];
+    for (i = 0; i < 4; i++) {
+      col = [grid[0][i], grid[1][i], grid[2][i], grid[3][i], ].filter((value) => value !== 0);
+      for (let j = col.length; j > 0; j--) {
+        if (col[j] === col[j - 1]) {
+          col[j] *= 2;
+          col[j - 1] = 0;
+          score += col[j];
+        }
+      }
+      col.filter((value) => value !== 0);
+      while (col.length < 4) col.unshift(0);
+      for (j = 0; j < col.length; j++) {
+        if (grid[i][j] !== col[j]) moved = true;
+        grid[j][i] = col[j];
+      }
+    }
+  }
+  
+  return moved;
+}
+
+function handleTouch() {
+  const diffX = touchStartX - touchEndX;
+  const diffY = touchStartY - touchEndY;
+  
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    if (diffX < -30)
+      move("right")
+    
+    else if (diffX > 30)
+      move("left");
+  }
+  else {
+    if (diffY > 30)
+      move("up")
+    else if (diffY < -30)
+      move("down");
+  }
+  addRandomTile();
+  updateGrid();
+}
+
+function updateGrid() {
+  gridElement.innerHTML = '';
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      const tile = document.createElement("div")
+      tile.className = `tile tile-${grid[i][j] || 0}`
+      tile.textContent = grid[i][j] || '';
+      gridElement.appendChild(tile)
+    }
+    
+    scoreCount.textContent = score;
+    if (isGameOver()) {
+      gameOver.style.visibility = 'visible'
+      finalScore.textContent = 'Score: ' + score;
+    }
+  }
+}
+
+function isGameOver() {
+  for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      if(grid[i][j] === 0) return false;
+      if(j < 3 && grid[i][j] === grid[i][j + 1]) return false;
+      if(i < 3 && grid[i][j] === grid[i + 1][j]) return false;
+    }
+  }
+  return true;
+}
+
+function restartGame() {
+  gameOver.style.visibility = 'hidden'
+  startGame();
+}
+startGame()
